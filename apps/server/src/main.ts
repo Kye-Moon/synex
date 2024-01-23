ef6487842f607923f00a220854348e6f8e120b08
KyeMoon123 <kyemoondevelopment@gmail.com>
2024-01-24 10:49:10 +1100

    deploy

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -7,7 +7,10 @@ const cookieParser = require('cookie-parser');
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3001','http://localhost:3000', 'http://localhost:4000','https://webui-szl9.onrender.com'],
+    origin:
+        ['http://localhost:3001','http://localhost:3000', 'http://localhost:4000',
+          'https://webui-szl9.onrender.com','https://production-varify-server.onrender.com/','https://production-varify-server.onrender.com',
+          'https://varify.synex.one'],
     credentials: true,
   })
 

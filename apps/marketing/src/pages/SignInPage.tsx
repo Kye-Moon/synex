a4c446c536f70951ac59d551ec2c8b5a421f9e5c
KyeMoon123 <kyemoondevelopment@gmail.com>
2024-02-28 23:34:44 +1100

    fix image import

--- apps/marketing/src/pages/SignInPage.tsx
+++ apps/marketing/src/pages/SignInPage.tsx
@@ -1,7 +1,7 @@
 import { SignIn } from "@clerk/clerk-react";
 
 const SignInPage = () => (
-    <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
+    <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" afterSignInUrl={window.origin.toString() + "/dashboard"} />
 );
 
 export default SignInPage;
